10728eafba2ea14043612e83c63b4af75be6d5e6
MouadhChebbi <mouadhnwa@hotmail.com>
2026-02-26 21:52:56 +0100

    add the reset password function in ResetpasswordController

--- app/Http/Controllers/ResetPwdController.php
+++ app/Http/Controllers/ResetPwdController.php
@@ -7,5 +7,20 @@ use App\Models\User;
 
 class ResetPwdController extends Controller
 {
-    //
+    function resetPassword(Request $request)
+    {
+        $user = User::where('email', $request->input('email'))->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found'
+            ], 404);
+        }
+        $user->password = bcrypt($request->input('new_password'));
+        $user->save();
+
+        return response()->json([
+            'message' => 'Password reset successful',
+            'user' => $user
+        ], 200);
+    }
 }
