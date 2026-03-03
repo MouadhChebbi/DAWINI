3d9642b649eac8c407bdabe9e0535fc9647f61c4
MouadhChebbi <mouadhnwa@hotmail.com>
2026-03-03 16:14:42 +0100

    added Login page

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use app\Models\File;
+use App\Models\File;
 
 class User extends Authenticatable
 {
